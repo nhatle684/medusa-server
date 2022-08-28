d1cce48bbb3ff5faaa99f82979cfd200ae9b14af
n.leq <quangnhat684@gmail.com>
2022-08-28 15:55:47 +0700

    Init

---
+++ medusa-config.js
@@ -0,0 +1,87 @@
+const dotenv = require('dotenv')
+
+let ENV_FILE_NAME = '';
+switch (process.env.NODE_ENV) {
+	case 'production':
+		ENV_FILE_NAME = '.env.production';
+		break;
+	case 'staging':
+		ENV_FILE_NAME = '.env.staging';
+		break;
+	case 'test':
+		ENV_FILE_NAME = '.env.test';
+		break;
+	case 'development':
+	default:
+		ENV_FILE_NAME = '.env';
+		break;
+}
+
+try {
+	dotenv.config({ path: process.cwd() + '/' + ENV_FILE_NAME });
+} catch (e) {
+}
+
+// CORS when consuming Medusa from admin
+const ADMIN_CORS = process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
+
+// CORS to avoid issues when consuming Medusa from a client
+const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
+
+// Database URL (here we use a local database called medusa-development)
+const DATABASE_URL =
+  process.env.DATABASE_URL || "postgres://localhost/medusa-store";
+
+// Medusa uses Redis, so this needs configuration as well
+const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
+
+// Stripe keys
+const STRIPE_API_KEY = process.env.STRIPE_API_KEY || "";
+const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET || "";
+
+// Cloudinary
+const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME || "";
+const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY || "";
+const CLOUDINARY_YOUR_API_SECRET = process.env.CLOUDINARY_YOUR_API_SECRET || "";
+
+// misc
+const COOKIE_SECRET = process.env.COOKIE_SECRET || "";
+const JWT_SECRET = process.env.JWT_SECRET || "";
+
+// This is the place to include plugins. See API documentation for a thorough guide on plugins.
+const plugins = [
+  `medusa-fulfillment-manual`,
+  `medusa-payment-manual`,
+  // Uncomment to add Stripe support.
+  // You can create a Stripe account via: https://stripe.com
+  // {
+  //   resolve: `medusa-payment-stripe`,
+  //   options: {
+  //     api_key: STRIPE_API_KEY,
+  //     webhook_secret: STRIPE_WEBHOOK_SECRET,
+  //   },
+  // },
+  {
+    resolve: `medusa-file-cloudinary`,
+    options: {
+        cloud_name: CLOUDINARY_CLOUD_NAME,
+        api_key: CLOUDINARY_API_KEY,
+        api_secret: CLOUDINARY_YOUR_API_SECRET,
+        secure: true,
+    },
+  },
+];
+
+module.exports = {
+  projectConfig: {
+    redis_url: REDIS_URL,
+    // For more production-like environment install PostgresQL
+    database_url: DATABASE_URL,
+    database_type: "postgres",
+    store_cors: STORE_CORS,
+    admin_cors: ADMIN_CORS,
+    jwt_secret: JWT_SECRET,
+    cookie_secret: COOKIE_SECRET,
+  },
+  plugins,
+};
